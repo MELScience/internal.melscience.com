134b085646b46de0156bdd8fee687cbb06394905
vassiliphilippov-mel <vassili@melscience.com>
2015-08-23 14:58:24 +0300

    Added date to done tasks

--- melchemistryprocess/js/melprocess.js
+++ melchemistryprocess/js/melprocess.js
@@ -483,6 +483,23 @@ MelTaskTimer.prototype.getFinishTimeString = function() {
         ":" + this.convertNumberToTwoDigitString(this.finishTime.getMinutes());
 }
 
+MelTaskTimer.prototype.getDateString = function() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if (dd<10) {
+    dd ='0' + dd;
+  } 
+
+  if (mm<10) {
+    mm = '0' + mm;
+  } 
+
+  return dd + '/' + mm + '/' + yyyy;
+}
+
 var melTaskTimer = new MelTaskTimer();
 
 ////////////////////////////////////////////////////////////////
